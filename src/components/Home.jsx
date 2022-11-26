f2fe1dbf0c7674cedc5abdd43d4b9737038352b7
Tommy Seamans <tlseamans1214@gmail.com>
2022-11-26 15:20:10 -0800

    Setup app and first 2 pages

---
+++ src/components/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Home() {
+    return (
+        <>
+            <div className="back-button">&#8249;</div>
+            <section className="menu">
+                <h1 className="menu-component menu-title">Groupmate</h1>
+                <div className="menu-component menu-button">Login</div>
+                <div className="menu-component menu-button">Sign Up</div>
+            </section>
+        </>
+    );
+}
+
+export default Home;
---
+++ src/components/Login.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Login() {
+    return (
+        <>
+            <div className="back-button">&#8249;</div>
+            <section className="menu">
+                <h1 className="menu-component menu-title">Groupmate</h1>
+                <h2 className="menu-component menu-subtitle">Login</h2>
+                <form className="login-form" action="/account-home">
+                    <label className="form-component" for="email">Email:</label>
+                    <input className="form-component" type="email" id="lemail" name="email" placeholder="email"></input>
+
+                    <label className="form-component" for="password">Password:</label>
+                    <input className="form-component" type="password" id="lpassword" name="password" placeholder="password"></input>
+  
+                    <input className="form-component submit-button" type="submit" value="Login"></input>
+                </form>
+            </section>
+        </>
+    );
+}
+
+export default Login;
